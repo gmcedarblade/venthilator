9bd8ab3b1118f977b9212194d18bcd9a2f96c149
Phildo <phildo211@gmail.com>
2017-05-14 15:44:44 -0500

    initial doodle commit

---
+++ src/scenes/gameplayScene.js
@@ -0,0 +1,26 @@
+var GamePlayScene = function(game, stage)
+{
+  var self = this;
+
+  var canv = stage.drawCanv;
+  var canvas = canv.canvas;
+  var ctx = canv.context;
+
+  self.ready = function()
+  {
+  };
+
+  self.tick = function()
+  {
+  };
+
+  self.draw = function()
+  {
+  };
+
+  self.cleanup = function()
+  {
+  };
+
+};
+
