ffef2fd4923d3dc3cfc51a83b82c25870b08572a
Phildo <phildo211@gmail.com>
2017-06-08 20:19:28 -0500

    variables in

--- src/scenes/gameplayScene.js
+++ src/scenes/gameplayScene.js
@@ -7,27 +7,88 @@ var GamePlayScene = function(game, stage)
   var ctx = canv.context;
   var n_ticks = 0;
   var cam = {wx:0,wy:0,ww:1,wh:2};
-  var my_graph;
-  var deriv_graph;
-  var commit_my_graph;
-  var commit_deriv_graph;
-  var my_data;
-  var deriv_data;
-  var my_knob;
+
+  //definition
+  var ENUM;
+  ENUM = 0;
+  var MODE_VOLUME   = ENUM; ENUM++;
+  var MODE_PRESSURE = ENUM; ENUM++;
+  ENUM = 0;
+  var IN_CHANNEL_MODE    = ENUM; ENUM++;
+  var IN_CHANNEL_RATE    = ENUM; ENUM++;
+  var IN_CHANNEL_FLOW    = ENUM; ENUM++;
+  var IN_CHANNEL_OXY     = ENUM; ENUM++;
+  var IN_CHANNEL_TIMEOUT = ENUM; ENUM++;
+  ENUM = 0;
+  var GRAPH_TYPE_VOLUME   = ENUM; ENUM++;
+  var GRAPH_TYPE_PRESSURE = ENUM; ENUM++;
+  var GRAPH_TYPE_FLOW     = ENUM; ENUM++;
+  ENUM = 0;
+  var GRAPH_BASE       = ENUM; ENUM++;
+  var GRAPH_DERIVATIVE = ENUM; ENUM++;
+
+  var min_in_volume = 1;
+  var max_in_volume = 2;
+  var min_in_pressure = 1;
+  var max_in_pressure = 2;
+  var min_in_rate = 1;
+  var max_in_rate = 2;
+  var min_in_flow = 1;
+  var max_in_flow = 2;
+  var min_in_oxy = 1;
+  var max_in_oxy = 2;
+  var min_in_timeout = 1;
+  var max_in_timeout = 2;
+
+  //data
+  var selected_mode = 0;
   var selected_channel = 0;
+  var alert_t = 0;
+  var in_volume = min_in_volume;
+  var in_pressure = min_in_pressure;
+  var in_rate = min_in_rate;
+  var in_flow = min_in_flow;
+  var in_oxy = min_in_oxy;
+  var in_timeout = min_in_timeout;
+  var patient_compliance = 1;
+  var patient_volume = 0;
+  var patient_pressure = 0;
+
+  //ui state
+  var selected_graph_type = 0;
+  var selected_graph_mode = 0;
+
+  //ui
+  var patient_volume_graph;
+  var deriv_graph;
+  var commit_patient_volume_graph;
+  var commit_deriv_graph;
+  var my_knob;
   var channel_btns;
+  var mode_btn;
+  var graph_type_btn;
+  var graph_mode_btn;
   var commit_btn;
-  var alert_t;
+
+  //filters
   var clicker;
   var dragger;
 
-  var ENUM;
-  ENUM = 0;
-  var CHANNEL_WAVELENGTH = ENUM; ENUM++;
-  var CHANNEL_AMPLITUDE  = ENUM; ENUM++;
-  var CHANNEL_SPACING    = ENUM; ENUM++;
-  var CHANNEL_OFFSET     = ENUM; ENUM++;
-  var CHANNEL_PULSE      = ENUM; ENUM++;
+  // MODES
+
+  //  lungs have different "compliance" (willingness to change in volume w/ increase of pressure)
+  //  patient triggers via detected negative pressure
+  //  also triggers via timeout
+
+  //AC (assist control) (CMV, continuous manditory venthilation) common
+  //  delivers specific volume
+  //  outputs pressure to watch changes in compliance
+
+  //PC (pressure control)
+  //  delivers specific pressure
+  //  outputs volume to watch changes in compliance
+
+  //commit
 
   var graph_data = function()
   {
@@ -137,6 +198,78 @@ var GamePlayScene = function(game, stage)
       self.data[self.data_pts-1] = self.data[self.data_pts-2];
     }
   }
+
+  var graph_set = function()
+  {
+    var self = this;
+    self.wx = 0;
+    self.wy = 0;
+    self.ww = 0;
+    self.wh = 0;
+    self.x = 0;
+    self.y = 0;
+    self.w = 0;
+    self.h = 0;
+
+    self.graph = new graph();
+    self.commit_graph = new graph();
+    self.deriv_graph = new graph();
+    self.commit_deriv_graph = new graph();
+
+    self.data = new graph_data();
+    self.data.gen_data();
+    self.deriv_data = new graph_data();
+    //self.deriv_data.min_y = -0.1;
+    //self.deriv_data.max_y = 0.1;
+
+    var apply_size_graph = function(g)
+    {
+      g.wx = self.wx;
+      g.wy = self.wy;
+      g.ww = self.ww;
+      g.wh = self.wh;
+      g.x = self.x;
+      g.y = self.y;
+      g.w = self.w;
+      g.h = self.h;
+      g.gen_cache();
+    }
+    self.apply_size = function()
+    {
+      apply_size_graph(self.graph);
+      apply_size_graph(self.commit_graph);
+      apply_size_graph(self.deriv_graph);
+      apply_size_graph(self.commit_deriv_graph);
+    }
+
+    self.update = function()
+    {
+      self.data.gen_data();
+      self.graph.consume_data(self.data);
+      self.deriv_data.deriv_data(self.data);
+      self.deriv_graph.consume_data(self.deriv_data);
+    }
+    self.commit = function()
+    {
+      self.commit_graph.consume_data(self.data);
+      self.commit_deriv_graph.consume_data(self.deriv_data);
+    }
+
+    self.draw = function(deriv)
+    {
+      if(!deriv)
+      {
+        self.graph.draw();
+        self.commit_graph.draw();
+      }
+      if(deriv)
+      {
+        self.deriv_graph.draw();
+        self.commit_deriv_graph.draw();
+      }
+    }
+  }
+
   var graph = function()
   {
     var self = this;
@@ -198,78 +331,64 @@ var GamePlayScene = function(game, stage)
     }
   }
 
+  var genBtn = function(channel, title, x)
+  {
+    btn = new ButtonBox(0,0,0,0, function(){selected_channel = channel; })
+    btn.title = title;
+    btn.wx = x;
+    btn.wy = 0.1
+    btn.ww = 0.15;
+    btn.wh = 0.1;
+    screenSpace(cam,canv,btn);
+    channel_btns[channel] = btn;
+  }
+  var drawBtn = function(btn)
+  {
+    ctx.strokeRect(btn.x,btn.y,btn.w,btn.h);
+    ctx.fillText(btn.title,btn.x+2,btn.y+btn.h/2+5);
+  }
+
   self.ready = function()
   {
     cam.wh = isTo(canv.width,1,canv.height);
 
-    my_graph = new graph();
-    my_graph.wx = 0;
-    my_graph.wy = 0.3;
-    my_graph.ww = cam.ww-0.1;
-    my_graph.wh = 0.2;
-    screenSpace(cam,canv,my_graph);
-    my_graph.gen_cache();
+    patient_volume_graph = new graph_set();
+    patient_volume_graph.wx = 0;
+    patient_volume_graph.wy = 0.3;
+    patient_volume_graph.ww = cam.ww-0.1;
+    patient_volume_graph.wh = 0.2;
+    screenSpace(cam,canv,patient_volume_graph);
+    patient_volume_graph.apply_size();
 
-    deriv_graph = new graph();
-    deriv_graph.wx = 0;
-    deriv_graph.wy = 0.52;
-    deriv_graph.ww = cam.ww-0.1;
-    deriv_graph.wh = 0.2;
-    screenSpace(cam,canv,deriv_graph);
-    deriv_graph.gen_cache();
-
-    commit_my_graph = new graph();
-    commit_my_graph.wx = my_graph.wx;
-    commit_my_graph.wy = my_graph.wy;
-    commit_my_graph.ww = my_graph.ww;
-    commit_my_graph.wh = my_graph.wh;
-    screenSpace(cam,canv,commit_my_graph);
-    commit_my_graph.gen_cache();
-
-    commit_deriv_graph = new graph();
-    commit_deriv_graph.wx = deriv_graph.wx;
-    commit_deriv_graph.wy = deriv_graph.wy;
-    commit_deriv_graph.ww = deriv_graph.ww;
-    commit_deriv_graph.wh = deriv_graph.wh;
-    screenSpace(cam,canv,commit_deriv_graph);
-    commit_deriv_graph.gen_cache();
-
-    my_data = new graph_data();
-    my_data.gen_data();
-    deriv_data = new graph_data();
-    deriv_data.min_y = -0.1;
-    deriv_data.max_y = 0.1;
-    deriv_data.deriv_data(my_data);
-    my_graph.consume_data(my_data);
-    deriv_graph.consume_data(deriv_data);
-    commit_my_graph.consume_data(my_data);
-    commit_deriv_graph.consume_data(deriv_data);
+    patient_volume_graph.update();
+    patient_volume_graph.commit();
 
     my_knob = new KnobBox(0,0,0,0, -1,1,0.1,0,function(v)
     {
       switch(selected_channel)
       {
-        case CHANNEL_WAVELENGTH:
-          my_data.wavelength = clamp(0.01,1,my_data.wavelength+v);
+        case IN_CHANNEL_MODE:
+               if(selected_mode == MODE_VOLUME)   in_volume   = clamp(min_in_volume,  max_in_volume,   in_volume  +v);
+          else if(selected_mode == MODE_PRESSURE) in_pressure = clamp(min_in_pressure,max_in_pressure, in_pressure+v);
+          patient_volume_graph.data.wavelength = clamp(0.01,1,patient_volume_graph.data.wavelength+v);
           break;
-        case CHANNEL_AMPLITUDE:
-          my_data.amplitude = clamp(0,1,my_data.amplitude+v);
+        case IN_CHANNEL_RATE:
+          in_rate = clamp(min_in_rate,max_in_rate,in_rate+v);
+          patient_volume_graph.data.spacing = clamp(0,1,patient_volume_graph.data.spacing+v);
           break;
-        case CHANNEL_SPACING:
-          my_data.spacing = clamp(0,1,my_data.spacing+v);
+        case IN_CHANNEL_FLOW:
+          in_flow = clamp(min_in_flow,max_in_flow,in_flow+v);
+          patient_volume_graph.data.amplitude = clamp(0,1,patient_volume_graph.data.amplitude+v);
           break;
-        case CHANNEL_OFFSET:
-          my_data.offset = clamp(-1,1,my_data.offset+v);
+        case IN_CHANNEL_OXY:
+          in_oxy = clamp(min_in_oxy,max_in_oxy,in_oxy+v);
           break;
-        case CHANNEL_PULSE:
-          my_data.delta_pulse(v);
+        case IN_CHANNEL_TIMEOUT:
+          in_timeout = clamp(min_in_timeout,max_in_timeout,in_timeout+v);
           break;
       }
       my_knob.val = 0;
-      my_data.gen_data();
-      deriv_data.deriv_data(my_data);
-      my_graph.consume_data(my_data);
-      deriv_graph.consume_data(deriv_data);
+      patient_volume_graph.update();
     });
     my_knob.wx = -0.3;
     my_knob.wy = -0.1;
@@ -279,28 +398,32 @@ var GamePlayScene = function(game, stage)
 
     channel_btns = [];
 
-    var genBtn = function(channel, title, x)
-    {
-      btn = new ButtonBox(0,0,0,0, function(){selected_channel = channel; })
-      btn.title = title;
-      btn.wx = x;
-      btn.wy = 0.1
-      btn.ww = 0.15;
-      btn.wh = 0.1;
-      screenSpace(cam,canv,btn);
-      channel_btns[channel] = btn;
-    }
-
     var x = -0.32;
     var s = 0.16;
-    genBtn(CHANNEL_WAVELENGTH,"wavelength",x); x += s;
-    genBtn(CHANNEL_AMPLITUDE,"amplitude",x); x += s;
-    genBtn(CHANNEL_SPACING,"spacing",x); x += s;
-    genBtn(CHANNEL_OFFSET,"offset",x); x += s;
-    genBtn(CHANNEL_PULSE,"pulse",x); x += s;
+    genBtn(IN_CHANNEL_MODE,   "Volume", x); x += s;
+    genBtn(IN_CHANNEL_RATE,   "Rate",   x); x += s;
+    genBtn(IN_CHANNEL_FLOW,   "Flow",   x); x += s;
+    genBtn(IN_CHANNEL_OXY,    "Oxygen", x); x += s;
+    genBtn(IN_CHANNEL_TIMEOUT,"Timeout",x); x += s;
 
-    commit_btn = new ButtonBox(0,0,0,0, function(){ commit_my_graph.consume_data(my_data); commit_deriv_graph.consume_data(deriv_data); })
-    commit_btn.title = "hello";
+    mode_btn = new ButtonBox(0,0,0,0, function(){ if(selected_mode == MODE_VOLUME) selected_mode = MODE_PRESSURE; else if(selected_mode == MODE_PRESSURE) selected_mode = MODE_VOLUME; })
+    mode_btn.title = "Mode";
+    mode_btn.wx = 0;
+    mode_btn.wy = -0.5
+    mode_btn.ww = 0.15;
+    mode_btn.wh = 0.1;
+    screenSpace(cam,canv,mode_btn);
+
+    graph_mode_btn = new ButtonBox(0,0,0,0, function(){ if(selected_graph_mode == GRAPH_BASE) selected_graph_mode = GRAPH_DERIVATIVE; else if(selected_graph_mode == GRAPH_DERIVATIVE) selected_graph_mode = GRAPH_BASE; })
+    graph_mode_btn.title = "Graph";
+    graph_mode_btn.wx = 0;
+    graph_mode_btn.wy = 0
+    graph_mode_btn.ww = 0.15;
+    graph_mode_btn.wh = 0.1;
+    screenSpace(cam,canv,graph_mode_btn);
+
+    commit_btn = new ButtonBox(0,0,0,0, function(){ patient_volume_graph.commit(); })
+    commit_btn.title = "Commit";
     commit_btn.wx = 0.3;
     commit_btn.wy = -0.5
     commit_btn.ww = 0.15;
@@ -319,10 +442,15 @@ var GamePlayScene = function(game, stage)
 
     for(var i = 0; i < channel_btns.length; i++)
       clicker.filter(channel_btns[i]);
+    clicker.filter(mode_btn);
+    clicker.filter(graph_mode_btn);
     clicker.filter(commit_btn);
     dragger.filter(my_knob);
     clicker.flush();
     dragger.flush();
+
+    if(selected_mode == MODE_VOLUME)   channel_btns[0].title = "Volume"
+    if(selected_mode == MODE_PRESSURE) channel_btns[0].title = "Pressure"
 
     var in_error = false;
     if(in_error) alert_t += 0.1;
@@ -331,18 +459,18 @@ var GamePlayScene = function(game, stage)
 
   self.draw = function()
   {
-    my_graph.draw();
-    deriv_graph.draw();
-    commit_my_graph.draw();
-    commit_deriv_graph.draw();
+    if(selected_graph_mode == GRAPH_BASE)
+      patient_volume_graph.draw();
+    else if(selected_graph_mode == GRAPH_DERIVATIVE)
+      patient_volume_graph.draw(1);
 
     ctx.fillStyle = "#000000"
     ctx.font = "10px Arial";
-    ctx.fillText(fdisp(my_data.wavelength),10,20);
-    ctx.fillText(fdisp(my_data.amplitude),40,20);
-    ctx.fillText(fdisp(my_data.spacing),70,20);
-    ctx.fillText(fdisp(my_data.offset),100,20);
-    ctx.fillText(fdisp(my_data.pulse_from_i+my_data.pulse_t),140,20);
+    ctx.fillText(fdisp(patient_volume_graph.data.wavelength),10,20);
+    ctx.fillText(fdisp(patient_volume_graph.data.amplitude),40,20);
+    ctx.fillText(fdisp(patient_volume_graph.data.spacing),70,20);
+    ctx.fillText(fdisp(patient_volume_graph.data.offset),100,20);
+    ctx.fillText(fdisp(patient_volume_graph.data.pulse_from_i+patient_volume_graph.data.pulse_t),140,20);
 
     ctx.fillStyle = "#AAAAAA";
     ctx.fillRect(channel_btns[selected_channel].x,channel_btns[selected_channel].y,channel_btns[selected_channel].w,channel_btns[selected_channel].h);
@@ -350,12 +478,11 @@ var GamePlayScene = function(game, stage)
     ctx.fillStyle = "#000000"
     ctx.font = "10px Arial";
     for(var i = 0; i < channel_btns.length; i++)
-    {
-      ctx.strokeRect(channel_btns[i].x,channel_btns[i].y,channel_btns[i].w,channel_btns[i].h);
-      ctx.fillText(channel_btns[i].title,channel_btns[i].x+2,channel_btns[i].y+channel_btns[i].h/2+5);
-    }
+      drawBtn(channel_btns[i]);
 
-    ctx.strokeRect(commit_btn.x,commit_btn.y,commit_btn.w,commit_btn.h);
+    drawBtn(mode_btn);
+    drawBtn(graph_mode_btn);
+    drawBtn(commit_btn);
 
     my_knob.draw(canv);
     ctx.font = "30px Arial";
